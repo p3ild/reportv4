17f1adf92cbc9406deb9ca06f34bf1d7bbcc3054
Nghĩa Nguyễn <nghianguyentran92@gmail.com>
2025-07-24 11:01:49 +0700

    test

--- src/instanceConfig/baocao25/index.js
+++ src/instanceConfig/baocao25/index.js
@@ -1,75 +1,72 @@
-import { PERIOD_TYPE } from '@core/ui/picker/periodpicker/periodpicker';
-import Base from '@core/base/ReportBase';
-import { getPickerStateByPath } from '@core/stateManage/corePickerState';
-import { classifyingOrgSelected } from './p2ild/common/request/utils';
-import locale from './locale.json';
+import { PERIOD_TYPE } from "@core/ui/picker/periodpicker/periodpicker";
+import Base from "@core/base/ReportBase";
+import { getPickerStateByPath } from "@core/stateManage/corePickerState";
+import { classifyingOrgSelected } from "./p2ild/common/request/utils";
+import locale from "./locale.json";
 export { locale };
 
 class Config extends Base {
-    // Server configuration
-    BASE_URL = 'https://dev.tkyt.vn/bc25';
-    classifyingOrgSelected = classifyingOrgSelected;
+  // Server configuration
+  BASE_URL = "https://dev.tkyt.vn/bc25";
+  classifyingOrgSelected = classifyingOrgSelected;
 
-    init = async () => {
-        getPickerStateByPath('actions.setAllowPeriodTypes')([
-            PERIOD_TYPE.year
-        ]);
-    }
+  init = async () => {
+    getPickerStateByPath("actions.setAllowPeriodTypes")([PERIOD_TYPE.year]);
+  };
 
-    listFolder = {
-        core: {
-            "key": "core",
-            "label": "Báo cáo cốt lõi ",
-            "child": []
-        },
+  listFolder = {
+    core: {
+      key: "core",
+      label: "Báo cáo cốt lõi ",
+      child: [],
+    },
 
-        other: {
-            "key": "other",
-            "label": "Báo cáo mẫu khác",
-            "child": []
-        }
-    }
+    other: {
+      key: "other",
+      label: "Báo cáo mẫu khác",
+      child: [],
+    },
+  };
 
-    listReport = [
-        {
-            key: "test",
-            folder: [this.listFolder.core],
-            displayName: 'test',
-            getReportInstance: async () => await import('./demo')
-        },
-        {
-            key: "eOlFpQQhgL4",
-            folder: [this.listFolder.core],
-            displayName: 'Thông tin chung',
-            getReportInstance: async () => await import('./p2ild/ttc')
-        },
-        {
-            folder: [this.listFolder.core],
-            key: "mMx0kzgX6Uv",
-            displayName: 'Hoạt động chăm sóc bà mẹ',
-            getReportInstance: async () => await import('./p2ild/bmte_b4')
-        },
-        {
-            folder: [this.listFolder.core],
-            key: "ctoSw0XuKp7",
-            displayName: 'TÌNH HÌNH MẮC VÀ TỬ VONG DO TAI BIẾN SẢN KHOA',
-            getReportInstance: async () => await import('./p2ild/bmte_b5')
-        },
-        {
-            folder: [this.listFolder.core],
-            key: "r2gK5JEiaxW",
-            displayName: 'HOẠT ĐỘNG KHÁM, CHỮA PHỤ KHOA VÀ SÀNG LỌC UNG THƯ CỔ TỬ CUNG',
-            getReportInstance: async () => await import('./p2ild/bmte_b6')
-        },
-        {
-            folder: [this.listFolder.core],
-            key: "xsJNZLJHG6u",
-            displayName: 'HOẠT ĐỘNG KHHGĐ VÀ PHÁ THAI',
-            getReportInstance: async () => await import('./p2ild/bmte_b7')
-        },
-    ];
-
-
+  listReport = [
+    {
+      key: "test",
+      folder: [this.listFolder.core],
+      displayName: "test",
+      getReportInstance: async () => await import("./demo"),
+    },
+    {
+      key: "eOlFpQQhgL4",
+      folder: [this.listFolder.core],
+      displayName: "Thông tin chung",
+      getReportInstance: async () => await import("./p2ild/ttc"),
+    },
+    {
+      folder: [this.listFolder.core],
+      key: "mMx0kzgX6Uv",
+      displayName: "Hoạt động chăm sóc bà mẹ",
+      getReportInstance: async () => await import("./p2ild/bmte_b4"),
+    },
+    {
+      folder: [this.listFolder.core],
+      key: "ctoSw0XuKp7",
+      displayName: "TÌNH HÌNH MẮC VÀ TỬ VONG DO TAI BIẾN SẢN KHOA",
+      getReportInstance: async () => await import("./p2ild/bmte_b5"),
+    },
+    {
+      folder: [this.listFolder.core],
+      key: "r2gK5JEiaxW",
+      displayName:
+        "HOẠT ĐỘNG KHÁM, CHỮA PHỤ KHOA VÀ SÀNG LỌC UNG THƯ CỔ TỬ CUNG",
+      getReportInstance: async () => await import("./p2ild/bmte_b6"),
+    },
+    {
+      folder: [this.listFolder.core],
+      key: "xsJNZLJHG6u",
+      displayName: "HOẠT ĐỘNG KHHGĐ VÀ PHÁ THAI",
+      getReportInstance: async () => await import("./p2ild/bmte_b7"),
+    },
+  ];
 }
 
-export default new Config
+export default new Config();
