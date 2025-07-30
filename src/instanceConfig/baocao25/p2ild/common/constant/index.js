0e2359a9c7847822e9c0c7b877b9d33fbbb9dbde
Nghĩa Nguyễn <nghianguyentran92@gmail.com>
2025-07-30 11:29:51 +0700

    update b11

--- src/instanceConfig/baocao25/p2ild/common/constant/index.js
+++ src/instanceConfig/baocao25/p2ild/common/constant/index.js
@@ -1,77 +1,76 @@
-import { cloneDeep } from "lodash"
+import { cloneDeep } from "lodash";
 
 export const ORG_GROUP = {
-    // ------------------------------------- BAOCAO25 -------------------------------------
-    TINH_DVHC: "mH8ggZyC39Z",
-    XA_DVHC: "uyuiasZ82O4",
-    XA_TYT: "OHWM3DxkeMR",
-    XA_CSYT_KHAC_TYT: "eHs95ggJw7J",
+  // ------------------------------------- BAOCAO25 -------------------------------------
+  TINH_DVHC: "mH8ggZyC39Z",
+  XA_DVHC: "uyuiasZ82O4",
+  XA_TYT: "OHWM3DxkeMR",
+  XA_CSYT_KHAC_TYT: "eHs95ggJw7J",
 
-    XA: "OHWM3DxkeMR",
-    XA_CSYT_KHAC: 'eHs95ggJw7J',
+  XA: "OHWM3DxkeMR",
+  XA_CSYT_KHAC: "eHs95ggJw7J",
 
-    TINH_YTTN: "yvriG1o5Xqh",
-    TUYEN_TINH: "iilcfvQ4zI1",
-    TUYEN_TRUNG_UONG: "UlhXS5yPKpZ",
-    TUYEN_XA: "fpnukMBDvUf",
+  TINH_YTTN: "yvriG1o5Xqh",
+  TUYEN_TINH: "iilcfvQ4zI1",
+  TUYEN_TRUNG_UONG: "UlhXS5yPKpZ",
+  TUYEN_XA: "fpnukMBDvUf",
 
-    TW_CSYT_CSSKBM: "WBaGiDFEZjL",
+  TW_CSYT_CSSKBM: "WBaGiDFEZjL",
 
-    TINH_CSYT_CONG_CSSKBM: 'ImtV96dR0JK',
-    TINH_YTTN_CSSKBM: 'KKrseu3RYOe'
+  TINH_CSYT_CONG_CSSKBM: "ImtV96dR0JK",
+  TINH_YTTN_CSSKBM: "KKrseu3RYOe",
 
+  // ------------------------------------- BAOCAO -------------------------------------
+  // HUYEN_DVHC: "W4U1KdFeIJH",
+  // HUYEN_DVHC_BF_2025: "BP9QnWc3imF",
+  // HUYEN_DVHC_AF_2025: "MCE5maEazYj",
+  // XA_DVHC_BF_2025: "RN21bo5CSCY",
+  // XA_DVHC_AF_2025: "ET3BlMSPD7x",
 
+  // TINH_353: {
+  //     INCLUDE: "ckwgZYOYF0q",
+  //     EXCLUDE: "mH8ggZyC39Z"
+  // },
 
-    // ------------------------------------- BAOCAO -------------------------------------
-    // HUYEN_DVHC: "W4U1KdFeIJH",
-    // HUYEN_DVHC_BF_2025: "BP9QnWc3imF",
-    // HUYEN_DVHC_AF_2025: "MCE5maEazYj",
-    // XA_DVHC_BF_2025: "RN21bo5CSCY",
-    // XA_DVHC_AF_2025: "ET3BlMSPD7x",
+  // HUYEN_353: {
+  //     INCLUDE: "AIcqlANFqtC",
+  //     EXCLUDE: "aVCye9sObKR"
+  // },
 
+  // XA_353: {
+  //     INCLUDE: "t9ajCzvEYRB",
+  //     EXCLUDE: "QW1NbMvLMjP"
+  // },
 
-    // TINH_353: {
-    //     INCLUDE: "ckwgZYOYF0q",
-    //     EXCLUDE: "mH8ggZyC39Z"
-    // },
-
-    // HUYEN_353: {
-    //     INCLUDE: "AIcqlANFqtC",
-    //     EXCLUDE: "aVCye9sObKR"
-    // },
-
-    // XA_353: {
-    //     INCLUDE: "t9ajCzvEYRB",
-    //     EXCLUDE: "QW1NbMvLMjP"
-    // },
-
-    // "Vùng_Bắc_Trung_Bộ_và_duyên_hải_miền_Trung": "cGDQ0sqLmFK",
-    // "Vùng_Trung_du_và_miền_núi_phía_Bắc": "tpWjiIDBUHI",
-    // "Vùng_Tây_Nguyên": "H5L0ZxyoJIW",
-    // "Vùng_Đông_Nam_Bộ": "cxi2i36k2VI",
-    // "Vùng_Đồng_bằng_sông_Cửu_Long": "vNu8mOy7Rdg",
-    // "Vùng_Đồng_bằng_sông_Hồng": "YpvY3ghB2WW",
-}
+  // "Vùng_Bắc_Trung_Bộ_và_duyên_hải_miền_Trung": "cGDQ0sqLmFK",
+  // "Vùng_Trung_du_và_miền_núi_phía_Bắc": "tpWjiIDBUHI",
+  // "Vùng_Tây_Nguyên": "H5L0ZxyoJIW",
+  // "Vùng_Đông_Nam_Bộ": "cxi2i36k2VI",
+  // "Vùng_Đồng_bằng_sông_Cửu_Long": "vNu8mOy7Rdg",
+  // "Vùng_Đồng_bằng_sông_Hồng": "YpvY3ghB2WW",
+};
 export const ORG_GROUP_SET = {
-    TUYEN: "VO3DdltSDqy",
-}
+  TUYEN: "VO3DdltSDqy",
+};
 
 export const ORG_SELECTED_TYPE = {
-    "COUNTRY": {
-        key: "COUNTRY",
-    },
+  COUNTRY: {
+    key: "COUNTRY",
+  },
 
-    "PROVINCE": { key: "PROVINCE" },
-    "PROVINCE_INDIVIDUAL": { key: "PROVINCE_INDIVIDUAL" },
+  PROVINCE: { key: "PROVINCE" },
+  PROVINCE_INDIVIDUAL: { key: "PROVINCE_INDIVIDUAL" },
 
-    "UNDER_MINISTRY": { key: "UNDER_MINISTRY" },
-    "UNDER_MINISTRY_INDIVIDUAL": { key: "UNDER_MINISTRY_INDIVIDUAL" },
+  UNDER_MINISTRY: { key: "UNDER_MINISTRY" },
+  UNDER_MINISTRY_INDIVIDUAL: { key: "UNDER_MINISTRY_INDIVIDUAL" },
 
-    "DIRECTLY_UNDER_MINISTRY": { key: "DIRECTLY_UNDER_MINISTRY" },
-    "DIRECTLY_UNDER_MINISTRY_INDIVIDUAL": { key: "DIRECTLY_UNDER_MINISTRY_INDIVIDUAL" },
+  DIRECTLY_UNDER_MINISTRY: { key: "DIRECTLY_UNDER_MINISTRY" },
+  DIRECTLY_UNDER_MINISTRY_INDIVIDUAL: {
+    key: "DIRECTLY_UNDER_MINISTRY_INDIVIDUAL",
+  },
 
-    "DISTRICT": { key: "DISTRICT" },
-    "DISTRICT_INDIVIDUAL": { key: "DISTRICT_INDIVIDUAL" },
+  DISTRICT: { key: "DISTRICT" },
+  DISTRICT_INDIVIDUAL: { key: "DISTRICT_INDIVIDUAL" },
 
-    "COMMUNE": { key: "COMMUNE" },
-}
+  COMMUNE: { key: "COMMUNE" },
+};
