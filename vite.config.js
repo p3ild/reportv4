563b5854b28c47870c12b5c7da80ad4e49f2da29
p2ild <duylam@hispvietnam.org>
2025-09-30 11:37:37 +0700

    core: modify vite.config.js and package.json are in compliance with refactor core

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from "path";
-import mkcert from 'vite-plugin-mkcert'
+import viteConfigSetup from './src/core/project.config/viteConfigSetup.js';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  const target = env.VITE_TARGET_BUILD_INSTANCE;
-  console.log(target)
-  return {
-    base: "./",
-    plugins: [
-      mkcert(),
-      react()
-    ],
-    resolve: {
-      alias: {
-        "@core": path.resolve(__dirname, "src/core"),
-        "@instanceConfig": path.resolve(__dirname, `src/instanceConfig/${target}`),
-      }
-    },
-    build: {
-      // sourcemap: true
-    },
-    server: {
-      https: true,
-      port: 3000,
-    },
-  }
+  return viteConfigSetup({ env, dirname: __dirname, mode });
 });
